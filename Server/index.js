91254ee438685cd9db19b32e8e04f0225ee4e7bc
Kuldeep Suthar <sutharkuldeep458@gmail.com>
2023-07-30 17:10:44 +0530

    added database config and initialzation

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config()
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('./public'))
+
 app.get('/', (req, res) => {
-    res.send('welcome to home page')
+    res.send('welcome to my website')
 })
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.PORT}`)
+app.get('/health', (req, res) => {
+    const healthcheck = {
+        uptime: process.uptime(),
+        responsetime: process.hrtime(),
+        message: 'OK',
+        timestamp: Date.now()
+    };
+    try {
+        res.send(healthcheck);
+    } catch (error) {
+        healthcheck.message = error;
+        res.status(503).send();
+    }
 })
+
+app.listen(process.env.PORT,
+    mongoose.connect(process.env.MONGODB)
+        .then(() => {
+            console.log(`Server running on http://localhost:${process.env.PORT}`)
+        })
+        .catch((err) => console.log(err))
+)
